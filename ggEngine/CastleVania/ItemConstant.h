dc7eec6be98ab13075182b3792a43c37df338277
umino <14520719@gm.uit.edu.vn>
2016-11-23 01:09:23 +0700

    item constant, weapon constant

---
+++ ggEngine/CastleVania/ItemConstant.h
@@ -0,0 +1,16 @@
+#pragma once
+class ItemConstant
+{
+public:
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+	static constexpr const char* SIMON_TEXTURE = "SimonTexture";
+};
+
