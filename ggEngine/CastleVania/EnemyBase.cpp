38619506b02cf4be4dd4293dfeb891dba0e1c57d
Phan Truong Thanh <14520848@gm.uit.edu.vn>
2016-11-27 17:55:30 +0700

    Add virtual function for enemy base

--- ggEngine/CastleVania/EnemyBase.cpp
+++ ggEngine/CastleVania/EnemyBase.cpp
@@ -1,9 +1,25 @@
 #include "EnemyBase.h"
-
+#include "Simon.h"
 EnemyBase::EnemyBase(CVGame * cvGame, SpriteInfo * image, int frameWidth, int frameHeight, int defaultFrame, int numberOfFrame, DWORD msPerFrame):CVSpriteAnimation(cvGame,image, frameWidth,frameHeight, defaultFrame, numberOfFrame, msPerFrame)
 {
 }
 
 EnemyBase::~EnemyBase()
+{
+}
+
+void EnemyBase::CheckCollisionToSimon(Simon * simon)
+{
+}
+
+void EnemyBase::OnSimonContact(Simon * simon, ColliderArg e)
+{
+}
+
+void EnemyBase::Destroy()
+{
+}
+
+void EnemyBase::Active()
 {
 }
