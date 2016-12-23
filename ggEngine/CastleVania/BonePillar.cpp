2384da9de6c2c95390f9e15180195d7c0086b0ff
Phan Truong Thanh <14520848@gm.uit.edu.vn>
2016-12-23 21:49:39 +0700

    no message

--- ggEngine/CastleVania/BonePillar.cpp
+++ ggEngine/CastleVania/BonePillar.cpp
@@ -3,6 +3,7 @@
 BonePillar::BonePillar(CVGame * cvGame, SpriteInfo * spriteInfo) : EnemyBase(cvGame,spriteInfo,16,32)
 {
 	this->body->SetActive(false);
+	this->SetAnchor(0.5, 0);
 }
 
 BonePillar::~BonePillar()
