6fd72e53c91da0ee596a56dbc35aac72db2f4f10
PHANTRUONGTHANH\knightofk9 <knightofk9@gmail.com>
2016-12-29 18:09:17 +0700

    Update

--- ggEngine/CastleVania/CVGame.cpp
+++ ggEngine/CastleVania/CVGame.cpp
@@ -9,6 +9,7 @@
 #include "AudioManager.h"
 #include "CVCamera.h"
 #include "EnemyManager.h"
+#include "CVDebugDefine.h"
 CVGame::CVGame(HWND hWnd, int width, int height, GameMode mode, PhysicsMode physicsMode, D3DCOLOR gameColor) :Game(hWnd, width, height, mode, physicsMode, gameColor)
 {
 	
@@ -34,134 +35,109 @@ GameObject * CVGame::GetObjectInstance(const char * objectJson,Group *group)
 	type = json["type"].GetString();
 	x = json["x"].GetDouble();
 	y = json["y"].GetDouble();
-
+	SAFE_BREAK_BEGIN
 	if (type == "TileBrick") {
 		width = json["width"].GetDouble();
 		height = json["height"].GetDouble();
 		go = this->staticTileManager->AddTileBrick(x, y, width, height, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "BreakableTileBrick") {
 		go = this->itemManager->AddBreakableTileBrick(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Random") {
 		go = this->itemManager->AddHeart(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Door") {
 		go = this->staticTileManager->AddDoor(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 
 	if (type == "TileLadderDownLeft") {
 		go = this->staticTileManager->AddTileLadder(x, y, ObjectType_LadderDownLeft , group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "TileLadderDownRight") {
 		go = this->staticTileManager->AddTileLadder(x, y, ObjectType_LadderDownRight, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "TileLadderUpLeft") {
 		go = this->staticTileManager->AddTileLadder(x, y, ObjectType_LadderUpLeft, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "TileLadderUpRight") {
 		go = this->staticTileManager->AddTileLadder(x, y, ObjectType_LadderUpRight, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Simon") {
 		go = this->cvAdd->CharSimon(x, y, CharacterConstant::SIMON_MAX_HEALTH , nullptr, nullptr, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "BigHeart") {
 		go = this->itemManager->AddHeartLarger(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Heart") {
 		go = this->itemManager->AddHeart(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "HolyWater") {
 		go = this->itemManager->AddHolyWater(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "InvincibilityPotion") {
 		go = this->itemManager->AddInvinPotion(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "MoneyBag100") {
 		go = this->itemManager->AddMoneyBag100(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "MoneyBag400") {
 		go = this->itemManager->AddMoneyBag400(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "MoneyBag700") {
 		go = this->itemManager->AddMoneyBag700(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "PotRoast") {
 		go = this->itemManager->AddPotRoast(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "StopWatch") {
 		go = this->itemManager->AddStopWatch(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "TripleShot") {
 		go = this->itemManager->AddTripleShot(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "WhipUpgrade") {
 		go = this->itemManager->AddWhipUpgrade(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Axe") {
 		go = this->itemManager->AddAxe(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Boomerang") {
 		go = this->itemManager->AddBoomerang(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Cross") {
 		go = this->itemManager->AddCross(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Dagger") {
 		go = this->itemManager->AddDagger(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "DoubleShot") {
 		go = this->itemManager->AddDoubleShot(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 
 
@@ -171,79 +147,67 @@ GameObject * CVGame::GetObjectInstance(const char * objectJson,Group *group)
 		auto candle = this->itemManager->AddFireCandle(x, y, group);
 		candle->SetDropItem(dropType);
 		go = candle;
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Crown") {
 		go = this->itemManager->AddCrown(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Stuff") {
 		go = this->itemManager->AddStuff(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "MoneyBag1000") {
 		go = this->itemManager->AddMoneyBag1000(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Raven") {
 		go = this->enemyManager->AddRaven(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Skeleton") {
 		go = this->enemyManager->AddSkeleton(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Boss") {
 		go = this->enemyManager->AddMedusa(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "BonePillar") {
 		go = this->enemyManager->AddBonePillar(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Ghost") {
 		go = this->enemyManager->AddGhost(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "AI7") {
 		go = this->enemyManager->AddAI7(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "AI6") {
 		go = this->enemyManager->AddAI6(x, y, json["width"].GetDouble(), group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "BatEnemy") {
 		go = this->enemyManager->AddBatEnemy(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "MedusaHead") {
 		go = this->enemyManager->AddMedusaHead(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "AxeKnight") {
 		go = this->enemyManager->AddAxeKnight(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
 	if (type == "Monkey") {
 		go = this->enemyManager->AddMonkey(x, y, group);
-		go->name = type;
-		return go;
+		SAFE_BREAK
 	}
+	SAFE_BREAK_END
+	go->name = type;
+	return go;
 }
 
 void CVGame::LoadComponent()
