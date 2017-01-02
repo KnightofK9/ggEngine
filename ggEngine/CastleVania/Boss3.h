170741a72575fbe623d7c8050d53e2d6f75c4a95
chán đời <14520848@gm.uit.edu.vn>
2017-01-02 12:10:12 +0700

    Update

--- ggEngine/CastleVania/Boss3.h
+++ ggEngine/CastleVania/Boss3.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "WalkingEnemyBase.h"
+class EnemySkillBase;
 class Boss3 : public WalkingEnemyBase {
 public:
 	Boss3(CVGame *cvGame, SpriteInfo* image, bool isPrimaryBoss);
 	~Boss3();
+
+	void Update() override;
 private:
+	void Fire(bool isLeft, Vector position);
+	void SetBullet(EnemySkillBase * bullet);
+	EnemySkillBase* bullet = nullptr;
+	int fireInterval;
+	Timer fireTimer;
 	bool isPrimaryBoss;
 };
